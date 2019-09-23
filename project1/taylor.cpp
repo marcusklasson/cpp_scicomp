676c61ff305d8c992c52e3db8b4d19baf6ead95a
marcusklasson <mklas@kth.se>
2019-09-23 14:45:29 +0200

    Fixed printing and added argument reduction

--- project1/taylor.cpp
+++ project1/taylor.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <stdio.h>
 #include "taylor.hpp"
 
 using namespace std;
@@ -8,10 +10,12 @@ using namespace std;
 
 double sinTaylor(int n, double x) {
 
+	//cout << "normalized angle: " << x - 2*M_PI*floor((x+M_PI)/(2*M_PI)) << endl;
 	// Think about how to make this more general!
-	if (x >= 2*M_PI) {
-		x = x - 2*M_PI;
-	}
+	// Must have this otherwise x= 10 gives overflow!
+	
+	// You must prove this one! Explain how and why you picked it?
+	x = x - 2*M_PI*floor((x + M_PI) / (2*M_PI) );
 
 	int sign = -1;
 	double sum, t;
@@ -28,10 +32,8 @@ double sinTaylor(int n, double x) {
 }
 
 double cosTaylor(int n, double x) {
-	// Think about how to make this more general!
-	if (x >= 2*M_PI) {
-		x = x - 2*M_PI;
-	}
+
+	x = x - 2*M_PI*floor((x + M_PI) / (2*M_PI) );
 
 	int sign = -1;
 	double sum, t;
@@ -46,31 +48,34 @@ double cosTaylor(int n, double x) {
 }
 
 int main() {
-	
-	// User input for N and x?
-	//cin >> x;
-	int n = 10;
-	//cout << "x \t cmath sin(x) \t my sin(x) \t my sin(x) 2" << endl;
-	cout << "x \t cmath sin(x) \t my sin(x) \t cmath cos(x) \t my cos(x)" << endl;
-	for (int i = -1; i <= 10; ++i) {
-		//cout << i << "\t" << sin(i) << "\t " << sinTaylor(n, i) << "\t " << sinTaylorAlternative(n, i) << endl;
-		cout << i << "\t" << sin(i) << "\t " << sinTaylor(n, i) << "\t" << cos(i) << "\t " << cosTaylor(n, i) << endl;
+
+	vector<int> x = {-1, 1, 2, 3, 5, 10, 15, 100}; // Values of x for sin(x) and cos(x)
+
+	int n;
+	cout << "Select number of terms in Taylor series: " << endl;
+	//cin >> n;
+	n = 10;
+
+	double error, bound;
+	double sinApprox, cosApprox;
+
+	cout << "x \t sin(x) \t sinTaylor(x) \t error \t\t bound \t" << endl;
+	for (int i = 0; i != x.size(); ++i) {
+		sinApprox = sinTaylor(n, x[i]);
+		error = abs(sin(x[i]) - sinApprox);
+		bound =  pow(-1.0, n+1) * sinApprox * (double)(i * i)/ ( (2*(n+1) + 1) * (2*(n+1) ) );
+
+		printf("%d \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n", x[i], sin(x[i]), sinApprox, error, abs(bound));
+		//cout << "normalized angle: " << x[i] - 2*M_PI*floor((x[i]+M_PI)/(2*M_PI)) << endl;
 	}
 
-	// TODO: fix how we compute the error bound!
-	double bound;
-	double sinApprox;
-	for (int i = -1; i <= 10; ++i) {
-		cout << "x \t cmath sin(x) \t my sin(x)" << endl;
-		//cout << i << "\t" << sin(i) << "\t " << sinTaylor(n, i) << "\t " << sinTaylorAlternative(n, i) << endl;
-		sinApprox = sinTaylor(n, i);
-		cout << i << "\t" << sin(i) << "\t " << sinApprox << endl;
-
-		bound =  pow(-1.0, n+1) * sinApprox * (double)(i * i)/ ( (2*(n+1) + 1) * (2*(n+1) ) );
-		cout << "Abs error \t n+1'st term" << endl;
-		cout << abs(sin(i) - sinApprox) << "\t " << abs(bound) << endl;
-	}	
-
+	cout << "\nx \t cos(x) \t cosTaylor(x) \t error \t\t bound \t" << endl;
+	for (int i = 0; i != x.size(); ++i) {
+		cosApprox = cosTaylor(n, x[i]);
+		error = abs(cos(x[i]) - cosApprox);
+		bound =  pow(-1.0, n+1) * cosApprox * (double)(i * i)/ ( (2*(n+1)) * (2*(n+1) - 1) );
+		printf("%d \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n", x[i], cos(x[i]), cosApprox, error, abs(bound));
+	}
 
 	return 0;
 }
