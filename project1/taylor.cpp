9fe98ff2c53b968a7be7f9ced627ceb93fc5692d
marcusklasson <mklas@kth.se>
2019-09-23 22:40:37 +0200

    Finished with code

--- project1/taylor.cpp
+++ project1/taylor.cpp
@@ -5,17 +5,15 @@
 #include "taylor.hpp"
 
 using namespace std;
-// Got help from:
-// https://math.stackexchange.com/questions/184302/create-a-c-program-to-evaluate-the-following-series-sin-x-approx-x-frac
+
+// Reduce x to the interval [-pi, pi)
+double argumentReduction(double radians) {
+	return radians - 2*M_PI*round(radians / (2*M_PI) );
+}
 
 double sinTaylor(int n, double x) {
 
-	//cout << "normalized angle: " << x - 2*M_PI*floor((x+M_PI)/(2*M_PI)) << endl;
-	// Think about how to make this more general!
-	// Must have this otherwise x= 10 gives overflow!
-	
-	// You must prove this one! Explain how and why you picked it?
-	x = x - 2*M_PI*floor((x + M_PI) / (2*M_PI) );
+	x = argumentReduction(x);
 
 	int sign = -1;
 	double sum, t;
@@ -24,16 +22,14 @@ double sinTaylor(int n, double x) {
 	for (int i = 1; i <= n; ++i) {
 		t = t * (x * x/((2*i + 1) * (2*i)) );
 		sum = sum + sign * t;
-		//cout << "sum: " << sum << endl;
 		sign = -sign;
 	} 
-    
 	return sum;
 }
 
 double cosTaylor(int n, double x) {
 
-	x = x - 2*M_PI*floor((x + M_PI) / (2*M_PI) );
+	x = argumentReduction(x);
 
 	int sign = -1;
 	double sum, t;
@@ -49,33 +45,34 @@ double cosTaylor(int n, double x) {
 
 int main() {
 
-	vector<int> x = {-1, 1, 2, 3, 5, 10, 15, 100}; // Values of x for sin(x) and cos(x)
-
+	vector<double> x = {-1, 1, 2, 3, 5, 10}; // Values of x for sin(x) and cos(x)
 	int n;
-	cout << "Select number of terms in Taylor series: " << endl;
-	//cin >> n;
-	n = 10;
+	cout << "\nSelect number of terms in Taylor series: ";
+	cin >> n;
 
-	double error, bound;
+	double sinx, cosx;
 	double sinApprox, cosApprox;
+	double error, bound;
 
 	printf("Computing Taylor series of sin(x) and cos(x) using %d terms.\n\n", n);
 	cout << "x \t sin(x) \t sinTaylor(x) \t error \t\t bound \t" << endl;
 	for (int i = 0; i != x.size(); ++i) {
+		sinx = sin(x[i]);
 		sinApprox = sinTaylor(n, x[i]);
-		error = abs(sin(x[i]) - sinApprox);
-		bound =  pow(-1.0, n+1) * sinApprox * (double)(i * i)/ ( (2*(n+1) + 1) * (2*(n+1) ) );
-
-		printf("%d \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n", x[i], sin(x[i]), sinApprox, error, abs(bound));
-		//cout << "normalized angle: " << x[i] - 2*M_PI*floor((x[i]+M_PI)/(2*M_PI)) << endl;
+		error = abs(sinx - sinApprox);
+		bound =  pow(-1.0, n+1) * sinApprox * (x[i] * x[i])/ ( (2*(n+1) + 1) * (2*(n+1) ) );
+		printf("%.f \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n",
+				 x[i], sinx, sinApprox, error, abs(bound));
 	}
 
 	cout << "\nx \t cos(x) \t cosTaylor(x) \t error \t\t bound \t" << endl;
 	for (int i = 0; i != x.size(); ++i) {
+		cosx = cos(x[i]);
 		cosApprox = cosTaylor(n, x[i]);
-		error = abs(cos(x[i]) - cosApprox);
-		bound =  pow(-1.0, n+1) * cosApprox * (double)(i * i)/ ( (2*(n+1)) * (2*(n+1) - 1) );
-		printf("%d \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n", x[i], cos(x[i]), cosApprox, error, abs(bound));
+		error = abs(cosx - cosApprox);
+		bound =  pow(-1.0, n+1) * cosApprox * (x[i] * x[i])/ ( (2*(n+1)) * (2*(n+1) - 1) );
+		printf("%.f \t %1.6f \t %1.6f \t %1.6e \t %1.6e \n",
+				 x[i], cosx, cosApprox, error, abs(bound));
 	}
 
 	return 0;
