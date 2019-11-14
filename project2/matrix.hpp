0215d656b09b6ca8061f3c7da3ba56564c9fcb3c
marcusklasson <mklas@kth.se>
2019-11-14 19:04:25 +0100

    added functions in hpp file

--- project2/matrix.hpp
+++ project2/matrix.hpp
@@ -1,10 +1,42 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <iostream>
+#include <vector>
+
 class Matrix {
 
+private:
+	unsigned mRows;
+	unsigned mCols;
+	std::vector<std::vector<double>> matrix;
+
 public:
-	Matrix(int m);
+	Matrix(unsigned m);
+	Matrix(unsigned m, unsigned n, double val);
+	Matrix(const Matrix &); // Copy constructor. 
+	//~Matrix(); // Do I need a destructor?
+
+	// from blog post
+	Matrix operator+(const Matrix&);
+	Matrix operator-(const Matrix&);
+	Matrix operator*(const Matrix&);
+	Matrix transpose();
+	Matrix operator+(double);
+	Matrix operator-(double);
+	Matrix operator*(double);
+	Matrix operator/(double);
+
+	//double& operator[](unsigned&, unsigned&);
+	unsigned int getRows() const;
+	unsigned int getCols() const; 
+
+	// From assignment sheet
+	Matrix& operator=(const Matrix&);
+	Matrix& operator+=(const Matrix&);
+	Matrix& operator*=(const Matrix&);
+	Matrix& operator*=(const double);
+	double norm() const;
 	void printMatrix() const;
 	void fillMatrix(double x=0.0);	
 
