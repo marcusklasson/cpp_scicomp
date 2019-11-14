823baaa9d0c4f3bdb9cbdae5ff71282c092f3104
marcusklasson <mklas@kth.se>
2019-11-14 19:04:56 +0100

    create and print matrix works

--- project2/matrix.cpp
+++ project2/matrix.cpp
@@ -1,20 +1,33 @@
-#include <iostream>
 #include "matrix.hpp"
 
 using namespace std;
 
-Matrix::Matrix(int m) {
-	double matrix[m][m] = {0};
+Matrix::Matrix(unsigned m) {
+	mRows = m;
+	mCols = m;
+	//double matrix[m][m] = {0};
+	matrix.reserve(mRows);
+	for (unsigned i = 0; i != mRows; ++i) {
+		vector<double> col(mCols, 0.0);
+		matrix.push_back(col);
+	}
 }
 
 void Matrix::printMatrix() const {
-	
+	for (unsigned i = 0; i != mRows; ++i) {
+		for (unsigned j = 0; j != mCols; ++j) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}	
 }
 
 
 int main() {
 	cout << "Create matrix from own class" << endl;
-	int m = 2;
+	unsigned m = 5;
 	Matrix matrix(m);
-	cout << "it worked?" << endl;
+	matrix.printMatrix();
+
+	return 0;
 }
