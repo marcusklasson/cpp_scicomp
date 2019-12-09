ac9c77a6a0b2fa1d3d086111b51673c39f0022ad
marcusklasson <mklas@kth.se>
2019-12-09 15:24:19 +0100

    added comment in x() and y()

--- project3/curvebase.cpp
+++ project3/curvebase.cpp
@@ -8,7 +8,7 @@ using namespace std;
 Curvebase::Curvebase() {} 
 
 double Curvebase::simpsonRule(double a, double b) {
-	return ((b - a) / 6.0) * ( dL(a) + 4 * dL((a+b)/2) + dL(b));
+	return ((b - a) / 6.0) * ( dL(a) + 4.0 * dL(0.5*(a+b)) + dL(b));
 }
 
 double Curvebase::dL(double p) {
@@ -25,15 +25,18 @@ double Curvebase::integrate(double a, double b) {
 	 * level and sum all the computed integrals on these 
 	 * partitions of the interval.
 	 */
-	double I, I1, I2, errest;
+	cout << "In integration... "<< endl;
+	double I = 0.0, I1, I2, errest;
 	double tol = 1e-6;
 	int node = 1;
 	while (true) {
 		I1 = simpsonRule(a, b);
 		I2 = simpsonRule(a, 0.5*(a+b)) + simpsonRule(0.5*(a+b), b);
 		errest = abs(I1 - I2);
+		//cout << "Node: " << node << endl;
 		if (errest < 15.0*tol) { // acceptable errest and in leaf
 			I += I2;
+			//cout << "Node: " << node << endl;
 			/* while odd node, traverse up
 			 * until first even node
 			 */
@@ -41,16 +44,16 @@ double Curvebase::integrate(double a, double b) {
 				if (node == 1) { // if root node
 					return I;
 				}
-				node = floor(0.5 * node);
-				a = 2 * a - b;
+				node = 0.5 * node;
+				a = (2 * a) - b;
 				tol *= 2; 
 			}
 			/* if even node, move up to parent node
 			 * and then go to right child node
 			 */
-			node = 0.5 * node;
-			b = 2 * b - a;
-			node = 2 * node + 1;
+			node = 0.5*node;
+			b = (2 * b) - a;
+			node = 2*node + 1;
 			a = 0.5 * (a+b);
 		} 
 		// go to left child node
@@ -64,13 +67,13 @@ double Curvebase::integrate(double a, double b) {
 }
 
 double Curvebase::newtonMethod(double p0, double s) {
-	double err = 1.0, tol = 1e-12;
+	double err = 1.0, tol = 1e-6;
 	int it = 0, maxit = 100;
 	double p, p1;
 	p = p0;
 
 	while (err > tol && it < maxit) {
-		p1 = p - integrate(a, p) / dL(p);
+		p1 = p - (integrate(a, p) - s*length) / dL(p);
 		err = fabs(p1 - p);
 		p = p1;
 		++it;
@@ -82,13 +85,16 @@ double Curvebase::newtonMethod(double p0, double s) {
 }
 
 double Curvebase::x(double s) {
+	// Could check that s is in [0, 1]
 	double p, p0;
 	p0 = a + s * length; // initial guess for newton's method
 	p = newtonMethod(p0, s);
+	cout << "From newtons method p = " << p << endl;
 	return xp(p);
 }
 
 double Curvebase::y(double s) {
+	// Could check that s is in [0, 1]
 	double p, p0;
 	p0 = a + s * length; // initial guess for newton's method
 	p = newtonMethod(p0, s);
