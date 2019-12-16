6c402be97894fa9756e9c1af66f292c13360e832
marcusklasson <mklas@kth.se>
2019-12-16 13:27:46 +0100

    changed yp since computed boundary wrong

--- project3/xcurve.cpp
+++ project3/xcurve.cpp
@@ -1,6 +1,8 @@
 #include <cmath>
 #include "xcurve.hpp"
 
+//#include <iostream>
+
 using namespace std;
 
 XCurve::XCurve(double x0, double x1) {
@@ -12,6 +14,8 @@ XCurve::XCurve(double x0, double x1) {
 
 // This member function looks awful
 double XCurve::xp(double p) {
+	//return p;
+	
 	if (p < a) {
 		return a;
 	} else if (p > b) {
@@ -19,9 +23,17 @@ double XCurve::xp(double p) {
 	} else {
 		return p;
 	}
+	
 }
 
 double XCurve::yp(double p) {
+	//cout << "p: " << p << endl;
+	if (p < -3) {
+		return 0.5 / (1 + exp(-3 * (p + 6)));
+	} else {
+		return 0.5 / (1 + exp(3 * p));
+	}
+	/*
 	if ((p < -3) && (p >= -10)) {
 		return 0.5 / (1 + exp(-3 * (p + 6)));
 	}
@@ -29,6 +41,7 @@ double XCurve::yp(double p) {
 		return 0.5 / (1 + exp(3 * p));
 	}
 	return p;
+	*/
 }
 
 double XCurve::dxp(double p) {
@@ -38,6 +51,8 @@ double XCurve::dxp(double p) {
 double XCurve::dyp(double p) {
 	if ((p < -3) && (p >= -10)) {
 		return 1.5 * (exp(-3*(p + 6))) / (1 + 2*exp(-3 * (p + 6)) + exp(-6 * (p + 6)));
+		//return 1.5 * (exp(3*(p + 6))) / (1 + 2*exp(3 * (p + 6)) + exp(6 * (p + 6)));
+		//return 1.5*exp(3.0*(p+6))/(1.0 + 2.0*exp(3.0*(p + 6.0)) + exp(6.0*(p+6.0)));
 	}
 	if ((p <= 5) && (p >= -3)) {
 		return -1.5 * (exp(3 * p) / (1 + 2*exp(3 * p) + exp(6 * p)) );
