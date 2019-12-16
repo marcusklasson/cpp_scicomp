4afb18bfcfd72801dcff9db245f01e247c841f77
marcusklasson <marcusklasson@hotmail.com>
2019-12-16 13:25:46 +0100

    added matlab code, plotted fig and binary file

---
+++ project3/plot_grid.m
@@ -0,0 +1,8 @@
+path = pwd;
+fileID = fopen([path, '/outfile.bin'], 'r');
+m = fread(fileID, 1, 'int');
+n = fread(fileID, 1, 'int');
+G = fread(fileID, [(m+1)*(n+1), 2], 'double');
+fclose(fileID);
+plot(G(:,1), G(:,2), '*')
+xlim([-10.5, 5.5]); ylim([-0.5, 3.5]);
